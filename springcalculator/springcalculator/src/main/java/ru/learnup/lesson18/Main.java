f1f8791b03e25d9fedff847a057624ed4cf7f143
CamillaMinyazeva <77393432+CamillaMinyazeva@users.noreply.github.com>
2022-05-16 23:46:15 +0300

    lesson18-2

---
+++ springcalculator/springcalculator/src/main/java/ru/learnup/lesson18/Main.java
@@ -0,0 +1,28 @@
+package ru.learnup.lesson18;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Calculator.class);
+
+    public static void main(String[] args) {
+
+        LOG.debug("Start our program...");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        System.out.println();
+        System.out.printf("Результат сложения 3 и 6: %d\n", calculator.sum(3, 6));
+        System.out.printf("Резултат вычитания 6 из 12: %d\n", calculator.subtract(12, 6));
+        System.out.printf("Результат деления 21 на 7: %d\n", calculator.divide(21, 7));
+        System.out.printf("Результат умножения 3 на 3: %d\n", calculator.multiply(3, 3));
+        System.out.println();
+
+        LOG.debug("Program ended successfully.");
+    }
+}
